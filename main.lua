c61fe04511b1bf7b1c4ad4abf2e3ecfba9451b06
nguyenvantung329 <nguyenvantung123453@gmail.com>
2026-02-01 15:17:53 +0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,7 @@
+print("GitHub Roblox script loaded successfully!")
+
+game.StarterGui:SetCore("SendNotification", {
+    Title = "GitHub OK",
+    Text = "Script load từ GitHub thành công",
+    Duration = 5
+})
